0990bf797913a6ba274c61f45145079d4157dae6
panromicsword <infantselva2@gmail.com>
2024-01-23 19:45:21 +0530

    Committing the Procedural Program

---
+++ Procedural Program/Main.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class Main
+{
+	public static void main(String args[])
+	{
+		System.out.println("Open the Maggie packet");
+		System.out.println("Pour the Water");
+		System.out.println("Put the masala");
+		System.out.println("Boil for 2mins");
+		System.out.println("You food is ready");
+	}
+}
